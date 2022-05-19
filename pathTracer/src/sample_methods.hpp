100c8a3e5b834a33f26e13a4197fae5cf0276cbe
Jamie Temple <jamie-temple@live.de>
2022-05-19 13:15:00 +0200

    refactor: sample methods cpu and gpu

--- pathTracer/src/sample_methods.hpp
+++ pathTracer/src/sample_methods.hpp
@@ -1,7 +1,6 @@
 #ifndef PATH_TRACER_SAMPLE_METHODS_HPP
 #define PATH_TRACER_SAMPLE_METHODS_HPP
 
-
 #include "device.hpp"
 #include "Math.hpp"
 
@@ -10,14 +9,14 @@
 * - https://blog.selfshadow.com/publications/s2012-shading-course/burley/s2012_pbs_disney_brdf_notes_v3.pdf
 */
 
-__device__ vec2 sample_uniform_disk(const vec2& rand)
+__both__ vec2 sample_uniform_disk(const vec2& rand)
 {
     float phi{ two_pi * rand.y };
     float r{ owl::sqrt(rand.x) };
     return { r * owl::cos(phi),r * owl::sin(phi) };
 }
 
-__device__ vec2 sample_concentric_disk(const vec2& rand)
+__both__ vec2 sample_concentric_disk(const vec2& rand)
 {
     // re-scale rand to be between [-1,1]
     float dx{ 2.0f * rand.x - 1 };
@@ -42,7 +41,7 @@ __device__ vec2 sample_concentric_disk(const vec2& rand)
     return { r * owl::cos(phi),r * owl::sin(phi) };
 }
 
-__device__ vec3 sample_uniform_sphere(const vec2& rand)
+__both__ vec3 sample_uniform_sphere(const vec2& rand)
 {
     float z{ 1.0f - 2.0f * rand.x };
     float r{ owl::sqrt(owl::max(0.0f, 1.0f - z * z)) };
@@ -52,7 +51,7 @@ __device__ vec3 sample_uniform_sphere(const vec2& rand)
     return vec3{ x, y, z };
 }
 
-__device__ vec3 sample_cosine_hemisphere(vec2 const& rand)
+__both__ vec3 sample_cosine_hemisphere(vec2 const& rand)
 {
     // 1. sample unit circle and save position into rand.u, rand.v
     vec2 circle_points{sample_concentric_disk(rand)};
@@ -61,12 +60,12 @@ __device__ vec3 sample_cosine_hemisphere(vec2 const& rand)
     return vec3{ circle_points.x, circle_points.y, cos_theta };
 }
 
-__device__ float pdf_cosine_hemisphere(vec3 const& w_o, vec3 const& w_i)
+__both__ float pdf_cosine_hemisphere(vec3 const& w_o, vec3 const& w_i)
 {
     return absCosTheta(w_i) * inv_pi;
 }
 
-__device__ vec3 sample_uniform_hemisphere(vec2 const& rand)
+__both__ vec3 sample_uniform_hemisphere(vec2 const& rand)
 {
     float z{ rand.x };
     float r{ owl::sqrt(owl::max(0.0f, 1.0f - z * z)) };
@@ -76,10 +75,9 @@ __device__ vec3 sample_uniform_hemisphere(vec2 const& rand)
     return vec3{ x, y, z };
 }
 
-__device__ float pdf_uniform_hemisphere(vec3 const& w_o, vec3 const& w_i)
+__both__ float pdf_uniform_hemisphere(vec3 const& w_o, vec3 const& w_i)
 {
     return 0.5f * inv_pi;
 }
-
 
 #endif //PATH_TRACER_SAMPLE_METHODS_HPP
