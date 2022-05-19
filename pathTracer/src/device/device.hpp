19a242931061de4d99b2f310a92cc4f373e997e7
Jamie Temple <jamie-temple@live.de>
2022-05-19 13:51:47 +0200

    refactor: removed shared header

--- pathTracer/src/device/device.hpp
+++ pathTracer/src/device/device.hpp
@@ -2,7 +2,9 @@
 #define DEVICE_HPP
 #pragma once
 
-#include "../shared/shared.hpp"
+#include <cuda_runtime.h>
+#include "camera.hpp"
+#include "random.hpp"
 
 #define GET(RETURN, TYPE, BUFFER, ADDRESS)\
 {\
@@ -18,6 +20,79 @@ RETURN = ((TYPE*)BUFFER.data)[ADDRESS];\
 if (check) {::printf("Device Error (%d, %s): %s\n", __LINE__, __FILE__, msg); asm("trap;"); }
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+
+struct material_data
+{
+    enum class type
+    {
+        NONE = 0 << 0,
+        LAMBERT = 1 << 0,
+        DISNEY = 1 << 0,
+    };
+
+    type type{ type::NONE };
+    vec3  baseColor{ 0.8f, 0.8f, 0.8f };
+    float subsurface{ 0.0f };
+    vec3  subsurfaceRadius{ 1.0f, 0.2f, 0.1f };
+    vec3  subsurfaceColor{ 0.8f, 0.8f, 0.8f };
+    float metallic{ 0.0f };
+    float specular{ 0.5f };
+    float specularTint{ 1.0f };
+    float roughness{ 0.5f };
+    float sheen{ 0.0f };
+    float sheenTint{ 1.0f };
+    float clearcoat{ 0.0f };
+    float clearcoatGloss{ 0.03f };
+    float ior{ 1.45f };
+};
+
+struct light_data
+{
+    enum class type
+    {
+        NONE = 0 << 0,
+        MESH = 1 << 0
+    };
+
+    type type{ type::MESH };
+    vec3 color{ 1.f };
+    float intensity{ 1.f };
+    float exposure{ 0.f };
+    float falloff{ 2.0f };
+    bool use_surface_area = false;
+};
+
+struct launch_params_data
+{
+    int32_t max_path_depth;
+    int32_t max_samples;
+    Buffer material_buffer;
+    Buffer light_buffer;
+    OptixTraversableHandle world;
+    cudaTextureObject_t environment_map;
+    bool use_environment_map;
+};
+
+struct triangle_geom_data
+{
+    int32_t matId{ -1 };
+    int32_t lightId{ -1 };
+    ivec3* index;
+    vec3* vertex;
+    vec3* normal;
+};
+
+struct ray_gen_data
+{
+    uint32_t* fbPtr;
+    ivec2 fbSize;
+    camera_data camera;
+};
+
+struct miss_data
+{
+};
 
 enum class ScatterEvent
 {
