1b7dd7b6239f15550b9b035118efd39c10297577
Jamie Temple <jamie-temple@live.de>
2022-05-19 12:17:40 +0200

    refactor: removed includes dir

--- pathTracer/Main.cpp
+++ pathTracer/Main.cpp
@@ -1,5 +1,4 @@
 ﻿
-#include <pt/Types.hpp>
 #include <device/device.hpp>
 
 #include "camera.hpp"
@@ -14,6 +13,46 @@
 using namespace owl;
 
 extern "C" char device_ptx[];
+
+struct entity
+{
+    int32_t materialId{ -1 };
+    int32_t lightId{ -1 };
+};
+
+struct optix_data
+{
+    // make context and shader
+    OWLContext context;
+    OWLModule module;
+
+    // launchParams static accessible mem.
+    OWLLaunchParams launch_params;
+    uint32_t max_path_depth;
+    uint32_t max_samples;
+
+    // Programs
+    OWLRayGen ray_gen_program;
+    OWLMissProg miss_program;
+
+    // link between host and device
+    ivec2 buffer_size{ 1024 };
+    OWLBuffer frame_buffer;
+
+    // Geometry and mesh
+    OWLGeomType triangle_geom;
+    std::vector<OWLGeom> geoms;
+
+    // Group to handle geometry
+    OWLGroup world;
+
+    // Texture holding env. information
+    OWLTexture environment_map;
+    bool use_environment_map;
+};
+
+static optix_data od{};
+
 
 void optix_init()
 {
