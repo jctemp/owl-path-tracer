97c299feeee854d708939de923500b57eaf0b78d
Jamie Temple <jamie-temple@live.de>
2022-05-20 16:56:33 +0200

    refactor: diffuse rewrite

--- pathTracer/src/device/materials.hpp
+++ pathTracer/src/device/materials.hpp
@@ -5,8 +5,8 @@
 #include "sample_methods.hpp"
 #include "device/bsdf_utils.hpp"
 #include "types.hpp"
+#include "macros.hpp"
 
-// REFECRENCES:
 // https://media.disneyanimation.com/uploads/production/publication_asset/48/asset/s2012_pbs_disney_brdf_notes_v3.pdf
 // https://blog.selfshadow.com/publications/s2015-shading-course/burley/s2015_pbs_disney_bsdf_notes.pdf
 
@@ -15,264 +15,259 @@ inline __both__ vec3 wh_flip(vec3 const& wo, vec3 const& wh)
     return same_hemisphere(wo, wh) ? wh : -wh;
 }
 
-__device__ vec3 fLambert(material_data const& mat, vec3 const& V, vec3 const& L)
+__device__ vec3 f_lambert(material_data const& m, vec3 const& wo, vec3 const& wi)
 {
-    if (!same_hemisphere(V, L)) return vec3{0.0f };
-    return mat.base_color * inv_pi;
+    return m.base_color * inv_pi;
 }
 
-__device__ float pdfLambert(material_data const& mat, vec3 const& V, vec3 const& L)
+__device__ float pdf_lambert(material_data const& m, vec3 const& wo, vec3 const& wi)
 {
-    if (!same_hemisphere(V, L)) return 0.0f;
-    float pdf{ 0.0f };
-    pdf_cosine_hemisphere(V, L);
-    return pdf;
+    return pdf_cosine_hemisphere(wo, wi);
 }
 
-__device__ void sampleLambert(material_data const& mat, vec3 const& V, vec3& L,
-                              Random& rand, vec3& bsdf, float& pdf)
+__device__ void sample_lambert(material_data const& m, vec3 const& wo, Random& rand, vec3& wi, vec3& f, float& pdf)
 {
-    sample_cosine_hemisphere({rand.random(), rand.random()});
-    pdf = pdfLambert(mat, V, L);
-    bsdf = fLambert(mat, V ,L);
+    sample_cosine_hemisphere(rand.random<vec2>());
+    pdf = pdf_lambert(m, wo, wi);
+    f = f_lambert(m, wo, wi);
 }
 
-__device__ vec3 fDisneyDiffuse(material_data const& mat, vec3 const& V, vec3 const& L)
+__device__ vec3 f_disney_diffuse(material_data const& m, vec3 const& wo, vec3 const& wi)
 {
-	float NdotV{ owl::abs(cos_theta(V)) };
-	float NdotL{ owl::abs(cos_theta(L)) };
+    // Burley 2015, eq (4).
+    auto const n_dot_wo{owl::abs(cos_theta(wo))};
+    auto const n_dot_wi{owl::abs(cos_theta(wi))};
 
-	float FL{schlick_fresnel(NdotL, mat.ior) };
-	float FV{schlick_fresnel(NdotV, mat.ior) };
+    auto const fresnel_wo{schlick_fresnel(n_dot_wo, m.ior)};
+    auto const fresnel_wi{schlick_fresnel(n_dot_wi, m.ior)};
 
-	// Burley 2015, eq (4).
-	return mat.base_color * inv_pi * (1.0f - FL / 2.0f) * (1.0f - FV / 2.0f);
+    return m.base_color * inv_pi * (1.0f - fresnel_wi / 2.0f) * (1.0f - fresnel_wo / 2.0f);
 }
 
 
-__device__ float pdfDisneyDiffuse(material_data const& mat, vec3 const& V, vec3 const& L)
+__device__ float pdf_disney_diffuse(material_data const& m, vec3 const& wo, vec3 const& wi)
 {
-	return pdf_cosine_hemisphere(V, L);
+    return pdf_cosine_hemisphere(wo, wi);
 }
 
 
-__device__ void sampleDisneyDiffuse(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
+__device__ void
+sample_disney_diffuse(material_data const& m, vec3 const& wo, Random& rand, vec3& wi, vec3& f, float& pdf)
 {
-    L = sample_cosine_hemisphere({rand.random(), rand.random()});
-	pdf = pdfDisneyDiffuse(mat, V, L);
-	bsdf = fDisneyDiffuse(mat, V, L) * owl::abs(cos_theta(L));
+    wi = sample_cosine_hemisphere({rand.random(), rand.random()});
+    pdf = pdf_disney_diffuse(m, wo, wi);
+    f = f_disney_diffuse(m, wo, wi) * owl::abs(cos_theta(wi));
 }
 
 __device__ vec3 fDisneyFakeSubsurface(material_data const& mat, vec3 const& V, vec3 const& L)
 // Hanrahan - Krueger BRDF approximation of the BSSRDF
 {
-	vec3 H{ L + V };
-	if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{ 0.0f };
+    vec3 H{L + V};
+    if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{0.0f};
 
-	H = normalize(H);
-	float cosThetaD{ dot(L, H) };
-	float NdotV{ owl::abs(cos_theta(V)) };
-	float NdotL{ owl::abs(cos_theta(L)) };
+    H = normalize(H);
+    float cosThetaD{dot(L, H)};
+    float NdotV{owl::abs(cos_theta(V))};
+    float NdotL{owl::abs(cos_theta(L))};
 
-	// Fss90 used to "flatten" retroreflection based on roughness
-	float Fss90{ cosThetaD * cosThetaD * mat.roughness };
-	float FL{schlick_fresnel(NdotL, mat.ior) };
-	float FV{schlick_fresnel(NdotV, mat.ior) };
-	float Fss{ lerp(1.0f, Fss90, FL) * lerp(1.0f, Fss90, FV) };
+    // Fss90 used to "flatten" retroreflection based on roughness
+    float Fss90{cosThetaD * cosThetaD * mat.roughness};
+    float FL{schlick_fresnel(NdotL, mat.ior)};
+    float FV{schlick_fresnel(NdotV, mat.ior)};
+    float Fss{lerp(1.0f, Fss90, FL) * lerp(1.0f, Fss90, FV)};
 
-	// 1.25 scale is used to (roughly) preserve albedo
-	float Fs{ 1.25f * (Fss * (1.0f / (NdotV + NdotL) - 0.5f) + 0.5f) };
+    // 1.25 scale is used to (roughly) preserve albedo
+    float Fs{1.25f * (Fss * (1.0f / (NdotV + NdotL) - 0.5f) + 0.5f)};
 
-	return mat.subsurface_color * inv_pi * Fs;
+    return mat.subsurface_color * inv_pi * Fs;
 }
 
 
 __device__ float pdfDisneyFakeSubsurface(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	return pdf_cosine_hemisphere(V, L);
+    return pdf_cosine_hemisphere(V, L);
 }
 
 
 __device__ void sampleDisneyFakeSubsurface(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
+                                           Random& rand, vec3& bsdf, float& pdf)
 {
     L = sample_cosine_hemisphere({rand.random(), rand.random()});
-	pdf = pdfDisneyFakeSubsurface(mat, V, L);
-	bsdf = fDisneyFakeSubsurface(mat, V, L) * owl::abs(cos_theta(L));
+    pdf = pdfDisneyFakeSubsurface(mat, V, L);
+    bsdf = fDisneyFakeSubsurface(mat, V, L) * owl::abs(cos_theta(L));
 }
 
 __device__ vec3 fDisneyRetro(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	vec3 H{ L + V };
-	if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{ 0.0f };
+    vec3 H{L + V};
+    if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{0.0f};
 
-	H = normalize(H);
-	float cosThetaD{ dot(L, H) };
-	float NdotV{ owl::abs(cos_theta(V)) };
-	float NdotL{ owl::abs(cos_theta(L)) };
+    H = normalize(H);
+    float cosThetaD{dot(L, H)};
+    float NdotV{owl::abs(cos_theta(V))};
+    float NdotL{owl::abs(cos_theta(L))};
 
-	float FL{schlick_fresnel(NdotL, mat.ior) };
-	float FV{schlick_fresnel(NdotV, mat.ior) };
-	float Rr{ 2 * mat.roughness * cosThetaD * cosThetaD };
+    float FL{schlick_fresnel(NdotL, mat.ior)};
+    float FV{schlick_fresnel(NdotV, mat.ior)};
+    float Rr{2 * mat.roughness * cosThetaD * cosThetaD};
 
-	// Burley 2015, eq (4).
-	return mat.base_color * inv_pi * Rr * (FL + FV + FL * FV * (Rr - 1));
+    // Burley 2015, eq (4).
+    return mat.base_color * inv_pi * Rr * (FL + FV + FL * FV * (Rr - 1));
 }
 
 
 __device__ float pdfDisneyRetro(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	return pdf_cosine_hemisphere(V, L);
+    return pdf_cosine_hemisphere(V, L);
 }
 
 
 __device__ void sampleDisneyRetro(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
+                                  Random& rand, vec3& bsdf, float& pdf)
 {
     L = sample_cosine_hemisphere({rand.random(), rand.random()});
-	pdf = pdfDisneyRetro(mat, V, L);
-	bsdf = fDisneyRetro(mat, V, L) * owl::abs(cos_theta(L));
+    pdf = pdfDisneyRetro(mat, V, L);
+    bsdf = fDisneyRetro(mat, V, L) * owl::abs(cos_theta(L));
 }
 
 __device__ vec3 fDisneySheen(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	vec3 H{ L + V };
-	if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{ 0.0f };
-	if (mat.sheen <= 0.0f) return vec3{ 0.0f };
+    vec3 H{L + V};
+    if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{0.0f};
+    if (mat.sheen <= 0.0f) return vec3{0.0f};
 
-	H = normalize(H);
-	float cosThetaD{ dot(L, H) };
+    H = normalize(H);
+    float cosThetaD{dot(L, H)};
 
-	vec3 tint{calculate_tint(mat.base_color) };
-	return mat.sheen * lerp(vec3{ 1.0f }, tint, vec3{ mat.sheen_tint }) * schlick_fresnel(cosThetaD, mat.ior);
+    vec3 tint{calculate_tint(mat.base_color)};
+    return mat.sheen * lerp(vec3{1.0f}, tint, vec3{mat.sheen_tint}) * schlick_fresnel(cosThetaD, mat.ior);
 }
 
 
 __device__ float pdfDisneySheen(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	return pdf_cosine_hemisphere(V, L);
+    return pdf_cosine_hemisphere(V, L);
 }
 
 
 __device__ void sampleDisneySheen(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
+                                  Random& rand, vec3& bsdf, float& pdf)
 {
     L = sample_cosine_hemisphere({rand.random(), rand.random()});
-	pdf = pdfDisneySheen(mat, V, L);
-	bsdf = fDisneySheen(mat, V, L) * owl::abs(cos_theta(L));
+    pdf = pdfDisneySheen(mat, V, L);
+    bsdf = fDisneySheen(mat, V, L) * owl::abs(cos_theta(L));
 }
 
 __device__ vec3 fDisneyClearcoat(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	vec3 H{ L + V };
-	if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{ 0.0f };
-	H = normalize(H);
+    vec3 H{L + V};
+    if (H.x == 0 && H.y == 0 && H.z == 0) return vec3{0.0f};
+    H = normalize(H);
 
-	// Clearcoat has ior = 1.5 hardcoded -> F0 = 0.04. It then uses the
-	// GTR1 distribution, which has even fatter tails than Trowbridge-Reitz
-	// (which is GTR2). The geometric term always based on alpha = 0.25
-	// - pbrt book v3
-	float alphaG{(1 - mat.clearcoat_gloss) * 0.1f + mat.clearcoat_gloss * 0.001f };
-	float Dr{d_gtr1(owl::abs(cos_theta(H)), alphaG) };
-	float F{schlick_fresnel(owl::abs(cos_theta(H)), 1.5f) };
-	float Fr{ lerp(.04f, 1.0f, F) };
-	float Gr{g_smith(owl::abs(cos_theta(V)), .25f) * g_smith(owl::abs(cos_theta(L)), .25f) };
+    // Clearcoat has ior = 1.5 hardcoded -> F0 = 0.04. It then uses the
+    // GTR1 distribution, which has even fatter tails than Trowbridge-Reitz
+    // (which is GTR2). The geometric term always based on alpha = 0.25
+    // - pbrt book v3
+    float alphaG{(1 - mat.clearcoat_gloss) * 0.1f + mat.clearcoat_gloss * 0.001f};
+    float Dr{d_gtr1(owl::abs(cos_theta(H)), alphaG)};
+    float F{schlick_fresnel(owl::abs(cos_theta(H)), 1.5f)};
+    float Fr{lerp(.04f, 1.0f, F)};
+    float Gr{g_smith(owl::abs(cos_theta(V)), .25f) * g_smith(owl::abs(cos_theta(L)), .25f)};
 
-	return mat.clearcoat * Gr * Fr * Dr / (4.0f * owl::abs(cos_theta(H)));
+    return mat.clearcoat * Gr * Fr * Dr / (4.0f * owl::abs(cos_theta(H)));
 }
 
 
 __device__ float pdfDisneyClearcoat(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	vec3 H{ L + V };
-	if (H.x == 0 && H.y == 0 && H.z == 0) return 0.0f;
-	H = normalize(H);
+    vec3 H{L + V};
+    if (H.x == 0 && H.y == 0 && H.z == 0) return 0.0f;
+    H = normalize(H);
 
-	// The sampling routine samples H exactly from the GTR1 distribution.
-	// Thus, the final value of the PDF is just the value of the
-	// distribution for H converted to a measure with respect to the
-	// surface normal.
-	// - pbrt book v3
-	float alphaG{(1 - mat.clearcoat_gloss) * 0.1f + mat.clearcoat_gloss * 0.001f };
-	float Dr{d_gtr1(owl::abs(cos_theta(H)), alphaG) };
-	return Dr * owl::abs(cos_theta(H)) / (4.0f * owl::abs(cos_theta(V)));
+    // The sampling routine samples H exactly from the GTR1 distribution.
+    // Thus, the final value of the PDF is just the value of the
+    // distribution for H converted to a measure with respect to the
+    // surface normal.
+    // - pbrt book v3
+    float alphaG{(1 - mat.clearcoat_gloss) * 0.1f + mat.clearcoat_gloss * 0.001f};
+    float Dr{d_gtr1(owl::abs(cos_theta(H)), alphaG)};
+    return Dr * owl::abs(cos_theta(H)) / (4.0f * owl::abs(cos_theta(V)));
 }
 
 
 __device__ void sampleDisneyClearcoat(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
-	// there is no visible normal sampling for BRDF because the Clearcoat has no
-	// physical meaning
-	// Apply importance sampling to D * dot(N * H) / (4 * dot(H, V)) by choosing normal
-	// proportional to D and reflect it at H
+                                      Random& rand, vec3& bsdf, float& pdf)
+// there is no visible normal sampling for BRDF because the Clearcoat has no
+// physical meaning
+// Apply importance sampling to D * dot(N * H) / (4 * dot(H, V)) by choosing normal
+// proportional to D and reflect it at H
 {
-	float alphaG{(1 - mat.clearcoat_gloss) * 0.1f + mat.clearcoat_gloss * 0.001f };
-	float alpha2{ alphaG * alphaG };
-	float cosTheta{ sqrtf(fmax(0.0f, (1 - powf(alpha2, 1 - rand.random())) / (1 - alpha2))) };
-	float sinTheta{ sqrtf(fmax(0.0f, 1 - cosTheta * cosTheta)) };
-	float phi{ two_pi * rand.random() };
+    float alphaG{(1 - mat.clearcoat_gloss) * 0.1f + mat.clearcoat_gloss * 0.001f};
+    float alpha2{alphaG * alphaG};
+    float cosTheta{sqrtf(fmax(0.0f, (1 - powf(alpha2, 1 - rand.random())) / (1 - alpha2)))};
+    float sinTheta{sqrtf(fmax(0.0f, 1 - cosTheta * cosTheta))};
+    float phi{two_pi * rand.random()};
 
-	vec3 H{to_sphere_coordinates(sinTheta, cosTheta, phi) };
+    vec3 H{to_sphere_coordinates(sinTheta, cosTheta, phi)};
 
-	if (!same_hemisphere(V, H)) H = -H;
+    if (!same_hemisphere(V, H)) H = -H;
 
-	bsdf = vec3{ 0.0f };
-	pdf = 0.0f;
+    bsdf = vec3{0.0f};
+    pdf = 0.0f;
 
-	L = reflect(V, H);
-	if (!same_hemisphere(V, L)) return;
+    L = reflect(V, H);
+    if (!same_hemisphere(V, L)) return;
 
-	pdf = pdfDisneyClearcoat(mat, V, L);
-	bsdf = fDisneyClearcoat(mat, V, L);
+    pdf = pdfDisneyClearcoat(mat, V, L);
+    bsdf = fDisneyClearcoat(mat, V, L);
 }
 
 __device__ vec3 fDisneyMicrofacet(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	if (!same_hemisphere(V, L)) return vec3{0.0f };
-	float NdotV{ owl::abs(cos_theta(V)) };
-	if (NdotV == 0) return vec3{ 0.0f };
+    if (!same_hemisphere(V, L)) return vec3{0.0f};
+    float NdotV{owl::abs(cos_theta(V))};
+    if (NdotV == 0) return vec3{0.0f};
 
-	vec3 H{ normalize(V + L) };
+    vec3 H{normalize(V + L)};
 
-	float alpha{to_alpha(mat.roughness) };
-	float Dr{d_gtr2(cos_theta(H), alpha) };
-	vec3 Fr{ lerp(mat.base_color, 1.0f - mat.base_color, {schlick_fresnel(dot(H, V), mat.ior)}) };
-	float Gr{g_smith(abs(tan_theta(V)), alpha) *
-             g_smith(abs(tan_theta(L)), alpha) };
+    float alpha{to_alpha(mat.roughness)};
+    float Dr{d_gtr2(cos_theta(H), alpha)};
+    vec3 Fr{lerp(mat.base_color, 1.0f - mat.base_color, {schlick_fresnel(dot(H, V), mat.ior)})};
+    float Gr{g_smith(abs(tan_theta(V)), alpha) *
+             g_smith(abs(tan_theta(L)), alpha)};
 
-	return Dr * Fr * Gr / (4.0f * owl::abs(cos_theta(L)));
+    return Dr * Fr * Gr / (4.0f * owl::abs(cos_theta(L)));
 }
 
 
 __device__ float pdfDisneyMicrofacet(material_data const& mat, vec3 const& V, vec3 const& L)
 {
-	if (!same_hemisphere(V, L)) return 0.0f;
+    if (!same_hemisphere(V, L)) return 0.0f;
 
-	vec3 H{ L + V };
-	if (H.x == 0 && H.y == 0 && H.z == 0) return 0.0f;
-	H = normalize(H);
+    vec3 H{L + V};
+    if (H.x == 0 && H.y == 0 && H.z == 0) return 0.0f;
+    H = normalize(H);
 
-	float alpha{to_alpha(mat.roughness) };
-	return pdf_gtr2(V, H, alpha);
+    float alpha{to_alpha(mat.roughness)};
+    return pdf_gtr2(V, H, alpha);
 }
 
 
 __device__ void sampleDisneyMicrofacet(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
+                                       Random& rand, vec3& bsdf, float& pdf)
 {
-	float alpha{to_alpha(mat.roughness) };
-	vec3 H{sample_gtr2_vndf(V, alpha, {rand.random(), rand.random()}) };
+    float alpha{to_alpha(mat.roughness)};
+    vec3 H{sample_gtr2_vndf(V, alpha, {rand.random(), rand.random()})};
 
-	L = reflect(V, H);
+    L = reflect(V, H);
 
-	bsdf = vec3{ 0.0f };
-	pdf = 0.0f;
+    bsdf = vec3{0.0f};
+    pdf = 0.0f;
 
-	if (!same_hemisphere(V, L)) return;
+    if (!same_hemisphere(V, L)) return;
 
-	bsdf = fDisneyMicrofacet(mat, V, L);
-	pdf = pdfDisneyMicrofacet(mat, V, L);
+    bsdf = fDisneyMicrofacet(mat, V, L);
+    pdf = pdfDisneyMicrofacet(mat, V, L);
 }
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -285,72 +280,72 @@ __device__ void sampleDisneyMicrofacet(material_data const& mat, vec3 const& V, 
 //
 
 __device__ void sampleDisneyBSDF(material_data const& mat, vec3 const& V, vec3& L,
-	Random& rand, vec3& bsdf, float& pdf)
+                                 Random& rand, vec3& bsdf, float& pdf)
 {
-	bsdf = vec3{ 0.0f };
-	pdf = 0.0f;
+    bsdf = vec3{0.0f};
+    pdf = 0.0f;
 
-	float r1 = rand.random(), r2 = rand.random();
+    float r1 = rand.random(), r2 = rand.random();
 
-	float specularWeight = mat.metallic;
-	float diffuseWeight = 1 - mat.metallic;
-	float clearcoatWeight = 1.0f * o_saturate(mat.clearcoat);
+    float specularWeight = mat.metallic;
+    float diffuseWeight = 1 - mat.metallic;
+    float clearcoatWeight = 1.0f * o_saturate(mat.clearcoat);
 
-	float norm = 1.0f / (clearcoatWeight + diffuseWeight + specularWeight);
+    float norm = 1.0f / (clearcoatWeight + diffuseWeight + specularWeight);
 
-	float pSpecular = specularWeight * norm;
-	float pDiffuse = diffuseWeight * norm;
-	float pClearcoat = clearcoatWeight * norm;
+    float pSpecular = specularWeight * norm;
+    float pDiffuse = diffuseWeight * norm;
+    float pClearcoat = clearcoatWeight * norm;
 
-	float cdf[3]{};
-	cdf[0] = pDiffuse;
-	cdf[1] = pSpecular + cdf[0];
-	cdf[2] = pClearcoat + cdf[1];
+    float cdf[3]{};
+    cdf[0] = pDiffuse;
+    cdf[1] = pSpecular + cdf[0];
+    cdf[2] = pClearcoat + cdf[1];
 
 
-	// diffuse
-	if (r1 < cdf[0])
-	{
-		vec3 H{ normalize(L + V) };
+    // diffuse
+    if (r1 < cdf[0])
+    {
+        vec3 H{normalize(L + V)};
 
-		if (H.z < 0.0f) H = -H;
+        if (H.z < 0.0f) H = -H;
 
         L = sample_cosine_hemisphere({rand.random(), rand.random()});
         pdf = pdf_cosine_hemisphere(V, L);
 
-		auto diffuse = fDisneyDiffuse(mat, V, L);
-		auto ss = fDisneyFakeSubsurface(mat, V, L);
-		auto retro = fDisneyRetro(mat, V, L);
-		auto sheen = fDisneySheen(mat, V, L);
+        auto diffuse = f_disney_diffuse(mat, V, L);
+        auto ss = fDisneyFakeSubsurface(mat, V, L);
+        auto retro = fDisneyRetro(mat, V, L);
+        auto sheen = fDisneySheen(mat, V, L);
 
-		auto diffuseWeight = 1 - mat.subsurface;
-		auto ssWeight = mat.subsurface;
+        auto diffuseWeight = 1 - mat.subsurface;
+        auto ssWeight = mat.subsurface;
 
-		bsdf += (diffuse + retro) * diffuseWeight;
-		bsdf += ss * ssWeight;
-		bsdf += sheen;
-		bsdf *= owl::abs(cos_theta(L));
-	}
+        bsdf += (diffuse + retro) * diffuseWeight;
+        bsdf += ss * ssWeight;
+        bsdf += sheen;
+        bsdf *= owl::abs(cos_theta(L));
+    }
 
-	// specular reflective
-	else if (r1 < cdf[1])
-	{
-		float alpha{to_alpha(mat.roughness) };
-		vec3 H{sample_gtr2_vndf(V, alpha, {rand.random(), rand.random()}) };
+        // specular reflective
+    else if (r1 < cdf[1])
+    {
+        float alpha{to_alpha(mat.roughness)};
+        vec3 H{sample_gtr2_vndf(V, alpha, {rand.random(), rand.random()})};
 
-		L = normalize(reflect(V, H));
+        L = normalize(reflect(V, H));
 
-		if (!same_hemisphere(V, L)) return;
+        if (!same_hemisphere(V, L)) return;
 
-		bsdf = fDisneyMicrofacet(mat, V, L);
-		pdf = pdfDisneyMicrofacet(mat, V, L);
-	}
+        bsdf = fDisneyMicrofacet(mat, V, L);
+        pdf = pdfDisneyMicrofacet(mat, V, L);
+    }
 
-	// clearcoat
-	else
-	{
-		sampleDisneyClearcoat(mat, V, L, rand, bsdf, pdf);
-	}
+        // clearcoat
+    else
+    {
+        sampleDisneyClearcoat(mat, V, L, rand, bsdf, pdf);
+    }
 
 }
 
